9d085c4b1512dc91a82a1efc5cb07b5ec9918d4f
Baylak <mongush.baylak17@gmail.com>
2023-09-11 20:47:09 +0200

    Add arg parser and checker file

--- parser.py
+++ parser.py
@@ -1,23 +1,48 @@
+import argparse
 import yaml
 import os
 
 
-# This class is used to parse the config.yml file and return the data as a dictionary
+# This class is used to parse the taskmaster.conf file and return the data as a dictionary
 class Parser:
 
     def __init__(self, file_path):
         self.file_path = file_path
 
     def parse(self):
-        if os.path.isfile(self.file_path):
+        if os.path.isfile(self.file_path) and self.file_path.lower().endswith('.conf'):
             with open(self.file_path, 'r') as stream:
                 try:
-                    return yaml.safe_load(stream)
+                    config_data = yaml.safe_load(stream)
+                    if config_data is not None and isinstance(config_data, dict):
+                        return config_data
+                    else:
+                        print("File content is not a valid YAML dictionary: " + self.file_path)
                 except yaml.YAMLError as exc:
-                    print(exc)
+                    print("Error parsing YAML: " + str(exc))
         else:
-            print("File not found: " + self.file_path)
+            print("Invalid or missing .conf file: " + self.file_path)
 
-# if __name__ == '__main__':
-#     parser = Parser('config.yml')
-#     print(parser.parse())
+
+def main():
+    # Create an argument parser
+    parser = argparse.ArgumentParser(description='Taskmaster Configuration Parser')
+
+    # Add an optional argument for specifying the configuration file
+    parser.add_argument('-c', '--config', metavar='CONFIG_FILE', help='Specify the .conf configuration file')
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    if args.config:
+        # If the -c flag is used, use the specified configuration file
+        parser = Parser(args.config)
+        print(parser.parse())
+    else:
+        # Otherwise, use the default 'taskmaster.conf' file
+        parser = Parser('taskmaster.conf')
+        print(parser.parse())
+
+
+if __name__ == '__main__':
+    main()
