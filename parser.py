35e217ff55de6f53d7e0c0dac5123cff68a1de36
Baylak <mongush.baylak17@gmail.com>
2023-09-15 20:53:13 +0200

    Add default paths

--- parser.py
+++ parser.py
@@ -10,7 +10,8 @@ class Parser:
         self.file_path = file_path
 
     def parse(self):
-        if os.path.isfile(self.file_path) and (self.file_path.lower().endswith('.yaml') or self.file_path.lower().endswith('.yml')):
+        if os.path.isfile(self.file_path) and \
+                (self.file_path.lower().endswith('.yaml') or self.file_path.lower().endswith('.yml')):
             with open(self.file_path, 'r') as stream:
                 try:
                     config_data = yaml.safe_load(stream)
@@ -31,23 +32,29 @@ class Parser:
 
 
 def main():
-    # Create an argument parser
-    parser = argparse.ArgumentParser(description='Taskmaster Configuration Parser')
+    default_config_paths = ['/etc/taskmaster.yaml', '/etc/taskmaster/taskmaster.yaml', '/etc/taskmaster.yml', '/etc/taskmaster/taskmaster.yml']
 
-    # Add an optional argument for specifying the configuration file
-    parser.add_argument('-c', '--config', metavar='CONFIG_FILE', help='Specify the .yaml or .yml configuration file')
-
-    # Parse the command-line arguments
+    parser = argparse.ArgumentParser(description='Taskmaster')
+    parser.add_argument('-c', '--config', type=str, help='Path to the configuration file')
     args = parser.parse_args()
 
-    if args.config:
-        # If the -c flag is used, use the specified configuration file
-        parser = Parser(args.config)
-        print(parser.parse())
+    if args.config is not None:
+        config_path = args.config
     else:
-        # Otherwise, use the default 'taskmaster.yaml' file
-        parser = Parser('taskmaster.yaml')
-        print(parser.parse())
+        for path in default_config_paths:
+            if os.path.isfile(path):
+                config_path = path
+                break
+        else:
+            print("No configuration file provided and no default configuration file found.")
+            return
+
+    config_parser = Parser(config_path)
+    config_data = config_parser.parse()
+    if config_data:
+        print("Configuration data loaded successfully.")
+    else:
+        print("Failed to load configuration data.")
 
 
 if __name__ == '__main__':
