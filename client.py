ce94e41579717130fe8db1ba041b082e696baff7
Baylak <mongush.baylak17@gmail.com>
2023-09-17 22:23:04 +0200

    Add client and server

--- client.py
+++ client.py
@@ -1,41 +1,94 @@
-import json, socket
+import json
+import socket
+
+
+def get_input(prompt, data_type, error_message):
+    while True:
+        try:
+            user_input = data_type(input(prompt))
+            return user_input
+        except ValueError:
+            print(error_message)
+
+
+def safe_input(prompt):
+    try:
+        return input(prompt)
+    except KeyboardInterrupt:
+        print("\nInput interrupted. Please try again.")
+        return None
+
+
+def validate_boolean_input(prompt):
+    while True:
+        user_input = input(prompt).lower()
+        if user_input == "true":
+            return True
+        elif user_input == "false":
+            return False
+        else:
+            print("Please enter 'true' or 'false'.")
+
+
+def validate_yes_no_input(prompt):
+    while True:
+        user_input = input(prompt).lower()
+        if user_input == "yes":
+            return True
+        elif user_input == "no":
+            return False
+        else:
+            print("Please enter 'yes' or 'no'.")
+
+
+def add_program(programs):
+    program_name = input("Enter program name: ")
+    programs[program_name] = {
+        "command": input("Enter command: "),
+        "numprocs": get_input("Enter numprocs: ", int, "Please enter a valid integer for numprocs."),
+        "autostart": validate_boolean_input("Enter autostart (true/false): "),
+        "autorestart": validate_boolean_input("Enter autorestart (true/false): "),
+        "exitcodes": [],
+        "startsecs": get_input("Enter startsecs: ", int, "Please enter a valid integer for startsecs."),
+        "startretries": get_input("Enter startretries: ", int, "Please enter a valid integer for startretries."),
+        "stopsignal": input("Enter stopsignal: "),
+        "stopwaitsecs": get_input("Enter stopwaitsecs: ", int, "Please enter a valid integer for stopwaitsecs."),
+        "stdout": input("Enter stdout: "),
+        "stderr": input("Enter stderr: "),
+        "environment_count": get_input("Enter environment_count: ", int,
+                                       "Please enter a valid integer for environment_count."),
+        "environment": {},
+        "workingdir": input("Enter workingdir: "),
+        "umask": input("Enter umask: ")
+    }
+
+    for i in range(programs[program_name]["environment_count"]):
+        key = input(f"Enter ENV_VAR{i + 1}: ")
+        value = safe_input(f"Enter value for ENV_VAR{i + 1}: ")
+        if value is None:
+            continue
+        programs[program_name]["environment"][key] = value
 
 
 def control_program():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(("localhost", 8888))
+    try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect(("localhost", 8888))
+    except ConnectionRefusedError:
+        print("Error: The server is not running or refused the connection.")
+        return
 
-    parameters = [
-        'command', 
-        'numprocs', 
-        'autostart',
-        'autorestart',
-        'exitcodes',
-        'startsecs',
-        'startretries',
-        'stopsignal',
-        'stopwaitsecs',
-        'stdout',
-        'stderr',
-        'environment_count',
-        'environment',
-        'ENV_VAR',
-        'workingdir',
-        'umask']
-
-    array = ""
+    programs = {}
 
     while True:
-        
-        for param in parameters:
-            array += "%s:" % param + input("Enter %s: " % param) + ";"
-        break
+        add_program(programs)
+        another_program = validate_yes_no_input("Add another program? (yes/no): ")
+        if not another_program:
+            break
 
-    data = json.dumps(array).encode('utf-8')
+    data = json.dumps({"programs": programs}).encode('utf-8')
     client_socket.send(data)
     print('Sent data')
-        
-        
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-import socket, json
+import socket
+import json
+import sys
 
 
 def daemon():
@@ -8,23 +10,49 @@ def daemon():
 
     print("Daemon is running and waiting for connections...")
 
-    while True:
-        client_socket, address = server_socket.accept()
-        print(f"Accepted connection from {address}")
-
+    try:
         while True:
-            data = client_socket.recv(4098)
-            if not data:
-                break
+            client_socket, address = server_socket.accept()
+            print(f"Accepted connection from {address}")
 
-            parametrs = json.loads(data)
+            while True:
+                data = client_socket.recv(4098)
+                if not data:
+                    break
 
-        client_socket.close()
-        print ('Received', parametrs)
-        
-        array = parametrs.split(";")
-        print (array)
-        for ()
+                program_data = json.loads(data.decode('utf-8'))
+
+                # Check if there are programs to process
+                if 'programs' in program_data:
+                    programs = program_data['programs']
+
+                    # Loop through each program
+                    for program_name, program_details in programs.items():
+                        print(f"Received program: {program_name}")
+
+                        # Process the "server" array if it exists
+                        if 'server' in program_details:
+                            server_array = program_details['server']
+                            print("Server array:")
+                            for server_item in server_array:
+                                print(server_item)
+
+                        # Process other program details as needed
+                        for key, value in program_details.items():
+                            if key == 'environment':
+                                print("Environment:")
+                                for env_key, env_value in value.items():
+                                    print(f"{env_key}: {env_value}")
+                            else:
+                                print(f"{key}: {value}")
+
+            client_socket.close()
+            print('Data received and processed')
+
+    except KeyboardInterrupt:
+        print("\nServer interrupted. Cleaning up and exiting...")
+        server_socket.close()
+        sys.exit(0)
 
 
 if __name__ == "__main__":
