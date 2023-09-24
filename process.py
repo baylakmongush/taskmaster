67faa9a24563d5738b96ffb1cdc7319769e9df6b
AloySobek <supernova.wandering.star@gmail.com>
2023-09-24 23:25:45 +0200

    WIP: Core runner is kinda done, need a lot of tests and it's time to start designing commands

--- process.py
+++ process.py
@@ -1,11 +1,13 @@
 import enum
 
+
 class Status(enum.Enum):
-    starting = 0
-    running = 1
-    stopping = 2
-    stopped = 3
-    failed = 4
+    none = 0
+    starting = 1
+    running = 2
+    stopping = 3
+    stopped = 4
+    failed = 5
 
 
 class RestartPolicy(enum.Enum):
@@ -20,40 +22,57 @@ class Process:
     pid: int
     restarts: int
     started_at: int
-    stopped_at: int
-    last_exit_code: int
 
-    owner: str
+    normal_exit_codes: list
+
+    def __init__(self, normal_exit_codes: list):
+        self.status = Status.none
+        self.pid = 0
+        self.restarts = 0
+        self.normal_exit_codes = normal_exit_codes
+
+    def __str__(self):
+        return ", ".join("%s: %s" % i for i in vars(self).items())
     
 
 class ProcessGroup:
     autostart: bool
-
     restart_policy: RestartPolicy
-
+    umask: int
     numprocs: int
-    max_numprocs: int
-    max_restarts: int
+    restarts: int
     initial_delay: int
     graceful_period: int
-    permissions: int
-    
     cwd: str
     exit_signal: str
     alternative_stdout: str
     alternative_stderr: str
-
     cmd: list
-    exit_codes: list
-
     env: dict
+    processes: list
 
-    processes: dict
+    def __init__(self, config: dict):
+        self.autostart = config["autostart"]
+        self.restart_policy = self._restart_policy_from_string(config["autorestart"])
+        self.umask = int(config["umask"], 8)
+        self.numprocs = config["numprocs"]
+        self.restarts = config["startretries"]
+        self.initial_delay = config["startsecs"]
+        self.graceful_period = config["stopwaitsecs"]
+        self.cwd = config["workingdir"]
+        self.exit_signal = config["stopsignal"]
+        self.alternative_stdout = config["stdout"]
+        self.alternative_stderr = config["stderr"]
+        self.cmd = config["command"].split()
+        self.env = config["environment"]
+        self.processes = [Process(config["exitcodes"]) for i in range(config["numprocs"])]
 
-    def __init__(self):
-        self.numprocs = 0
+    def _restart_policy_from_string(self, string):
+        for i in RestartPolicy:
+            if i.name == string:
+                return i
+
+        return None
 
     def __str__(self):
-        return f"Process group:\n\tautostart: {self.autostart}\n\trestart_policy: {self.restart_policy}\n\tnumprocs: {self.numprocs}\n\tmax_numprocs: {self.max_numprocs}\n\tmax_restarts: {self.max_restarts}\n\tinitial_delay: {self.initial_delay}\n\tgraceful_period: {self.graceful_period}\n\tpermissions: {oct(self.permissions)}\n\tcwd: {self.cwd}\n\texit_signal: {self.exit_signal}\n\talternative_stdout: {self.alternative_stdout}\n\talternative_stderr: {self.alternative_stderr}\n\tcmd: {self.cmd}\n\texit_codes: {self.exit_codes}\n\tenv: {self.env}"
-
-
+        return "\n".join("%s: %s" % (k, str([str(i) for i in v]) if isinstance(v, list) else v) for (k, v) in vars(self).items())
--- runner.py
+++ runner.py
@@ -4,110 +4,104 @@ import enum
 import time
 import parser
 import process
+import logging 
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+
+_logger = logging.getLogger(__name__)
 
 
-class State:
-    running: bool
+def move_cursor_up_and_clear(n=1):
+    for _ in range(n):
+        print("\033[K", end='')
+        print(f"\033[1A", end='')
 
+
+class Runner:
     process_groups: dict
     processes: dict
 
     def __init__(self):
-        self.running = True
         self.process_groups = dict()
         self.processes = dict()
 
+        config = parser.create_parser().parse()
 
-class Runner:
-    state: State
+        for k, v in config["programs"].items():
+            self.process_groups[k] = process.ProcessGroup(v)
 
-    parser: parser.Parser
-    data: dict
-
-    def __init__(self):
-        self.state = State()
-
-        self.parser = parser.create_parser()
-
-        self.data = self.parser.parse()
-
-        for k, v in self.data["programs"].items():
-            process_group = process.ProcessGroup()
-
-            process_group.autostart = v["autostart"]
-            process_group.restart_policy = self._restart_policy_from_string(v["autorestart"])
-            process_group.max_numprocs = v["numprocs"]
-            process_group.max_restarts = v["startretries"]
-            process_group.initial_delay = v["startsecs"]
-            process_group.graceful_period = v["stopwaitsecs"]
-            process_group.permissions = int(v["umask"], 8)
-            process_group.cwd = v["workingdir"]
-            process_group.exit_signal = v["stopsignal"]
-            process_group.alternative_stdout = v["stdout"]
-            process_group.alternative_stderr = v["stderr"]
-            process_group.cmd = v["command"].split()
-            process_group.exit_codes = v["exitcodes"]
-            process_group.env = v["environment"]
-
-            self.state.process_groups[k] = process_group
-
-            print(self.state.process_groups[k])
-
-    def poll_status(self):
+    def status(self):
         try:
             pid, exit_code = os.waitpid(-1, os.WNOHANG)
-            
-            if pid > 0:
-                print(f"Proccess {pid} exited with code {exit_code}")
 
-                p = self.state.processes[pid]
+            while pid > 0:
+                p = self.processes[pid]
 
-                self.state.process_groups[p.owner].numprocs -= 1
-
-                del self.state.processes[pid]
+                if exit_code in p.normal_exit_codes:
+                    p.status = process.Status.stopped
+                else:
+                    p.status = process.Status.failed
+                
+                pid, exit_code = os.waitpid(-1, os.WNOHANG)
         except ChildProcessError:
             pass
 
 
     def sync_state(self):
-        for k, process_group in self.state.process_groups.items():
-            while process_group.numprocs < process_group.max_numprocs:
-                pid = os.fork()
+        for g in self.process_groups.values():
+            for p in g.processes:
+                if p.restarts >= g.restarts:
+                    continue
 
-                if pid == 0:
-                    os.umask(process_group.permissions)
-                    os.chdir(process_group.cwd)
-                    os.execvp(process_group.cmd[0], process_group.cmd)
+                if p.status == process.Status.starting and time.time() - p.started_at > g.initial_delay:
+                    p.status = process.Status.running
+
+                if p.status == process.Status.none:
+                    self._run_process(g, p)
+                elif p.status == process.Status.stopped and g.restart_policy == process.RestartPolicy.always:
+                    self._run_process(g, p)
+                elif p.status == process.Status.failed and g.restart_policy == process.RestartPolicy.on_failure:
+                    self._run_process(g, p)
                 else:
-                    p = process.Process()
+                    pass
 
-                    p.status = process.Status.starting
-                    p.pid = pid
-                    p.restarts = 0
-                    p.started_at = time.time()
-                    p.stopped_at = None
-                    p.last_exit_code = 0
-                    p.owner = k
-
-                    process_group.processes[pid] = p
-                    self.state.processes[pid] = p
-
-                process_group.numprocs += 1
 
     def loop(self):
-        while (self.state.running):
-            self.poll_status()
+        while True:
+            self.status()
             self.sync_state()
+
+            for g in self.process_groups.values():
+                print(g)
+
+            move_cursor_up_and_clear(30)
 
             time.sleep(1)
 
+    def _run_process(self, g: process.ProcessGroup, p: process.Process):
+        if p.status != process.Status.none:
+            p.restarts += 1
 
-    def _restart_policy_from_string(self, string):
-        for i in process.RestartPolicy:
-            if i.name == string:
-                return i
+            del self.processes[p.pid]
 
-        return None
+        p.status = process.Status.starting
+
+        pid = os.fork()
+
+        if pid == 0:
+            sys.stdout = open(g.alternative_stdout)
+            sys.stderr = open(g.alternative_stderr)
+
+            os.chdir(g.cwd)
+
+            os.umask(g.umask)
+
+            os.execvpe(g.cmd[0], g.cmd, g.env)
+        else:
+            p.pid = pid
+            p.started_at = int(time.time())
+
+            self.processes[pid] = p;
 
 
 if __name__ == "__main__":
